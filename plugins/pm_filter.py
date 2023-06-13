563f02237d39aca007efbfcfeb4cd32f724d792e
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-13 11:22:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1421,7 +1421,7 @@ async def auto_filter(client, msg, spoll=False):
         )
     btn.insert(0, 
         [
-        InlineKeyboardButton(f'🌐 Languages', callback_data=f"select_lang#{req}")
+        InlineKeyboardButton(f'🌐 Languages', callback_data=f"select_lang#{message.from_user.id}")
         ],
     )        
     btn.insert(0, 
